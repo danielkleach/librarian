22624840775243c35ac63b73415701c35a7f9c8f
Daniel Leach <dleach@kimmel.com>
2017-08-25 16:54:23 -0400

    Adding api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,83 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+
+    Route::prefix('users')->group(function () {
+
+        Route::get('/', 'UserController@index');
+        Route::post('/', 'UserController@store');
+
+        Route::prefix('{userId}')->group(function () {
+
+            Route::get('/', 'UserController@show');
+            Route::patch('/', 'UserController@update');
+            Route::delete('/', 'UserController@destroy');
+        });
+    });
+
+    Route::prefix('categories')->group(function () {
+
+        Route::get('/', 'CategoryController@index');
+        Route::post('/', 'CategoryController@store');
+
+        Route::prefix('{categoryId}')->group(function () {
+
+            Route::get('/', 'CategoryController@show');
+            Route::patch('/', 'CategoryController@update');
+            Route::delete('/', 'CategoryController@destroy');
+        });
+    });
+
+    Route::prefix('authors')->group(function () {
+
+        Route::get('/', 'AuthorController@index');
+        Route::post('/', 'AuthorController@store');
+
+        Route::prefix('{authorId}')->group(function () {
+
+            Route::get('/', 'AuthorController@show');
+            Route::patch('/', 'AuthorController@update');
+            Route::delete('/', 'AuthorController@destroy');
+        });
+    });
+
+    Route::prefix('books')->group(function () {
+
+        Route::get('/', 'BookController@index');
+        Route::post('/', 'BookController@store');
+
+        Route::prefix('{bookId}')->group(function () {
+
+            Route::get('/', 'BookController@show');
+            Route::patch('/', 'BookController@update');
+            Route::delete('/', 'BookController@destroy');
+        });
+    });
+
+    Route::prefix('trackers')->group(function () {
+
+        Route::get('/', 'TrackerController@index');
+        Route::post('/', 'TrackerController@store');
+
+        Route::prefix('{trackerId}')->group(function () {
+
+            Route::get('/', 'TrackerController@show');
+            Route::patch('/', 'TrackerController@update');
+            Route::delete('/', 'TrackerController@destroy');
+        });
+    });
+
+    Route::prefix('reviews')->group(function () {
+
+        Route::get('/', 'UserReviewController@index');
+        Route::post('/', 'UserReviewController@store');
+
+        Route::prefix('{reviewId}')->group(function () {
+
+            Route::get('/', 'UserReviewController@show');
+            Route::patch('/', 'UserReviewController@update');
+            Route::delete('/', 'UserReviewController@destroy');
+        });
+    });
 });
