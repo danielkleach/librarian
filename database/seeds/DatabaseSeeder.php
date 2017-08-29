22f1af0037bbf31ccf6ec234e26e16c8ceda20ff
Daniel Leach <dleach@kimmel.com>
2017-08-29 11:03:02 -0400

    Updating seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Database\Seeds\StatusTypesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(AuthorsTableSeeder::class);
-        $this->call(StatusTypesTableSeeder::class);
         $this->call(BooksTableSeeder::class);
         $this->call(TrackersTableSeeder::class);
         $this->call(UserReviewsTableSeeder::class);
-        $this->call(StatusesTableSeeder::class);
     }
 }
