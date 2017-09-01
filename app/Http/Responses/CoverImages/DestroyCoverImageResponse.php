bd54a18f71d4058f20498b8848571f38a7a17997
Daniel Leach <dleach@kimmel.com>
2017-09-01 11:03:25 -0400

    Adding responses for cover images.

---
+++ app/Http/Responses/CoverImages/DestroyCoverImageResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class DestroyCoverImageResponse implements Responsable
+{
+    protected $coverImage;
+
+    public function __construct($coverImage)
+    {
+        $this->coverImage = $coverImage;
+    }
+
+    public function toResponse($request)
+    {
+        return response()->json(['message' => 'The cover image has been deleted.'], 204);
+    }
+}
---
+++ app/Http/Responses/CoverImages/StoreCoverImageResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class StoreCoverImageResponse implements Responsable
+{
+    protected $coverImage;
+
+    public function __construct($coverImage)
+    {
+        $this->coverImage = $coverImage;
+    }
+
+    public function toResponse($request)
+    {
+        return response()->json($this->transformCoverImage(), 201);
+    }
+
+    protected function transformCoverImage()
+    {
+        return ['cover_image_url' => $this->coverImage->url()];
+    }
+}
