19a92b69dfd95a6b41c5fd76adc488e2f147014d
Daniel Leach <dleach@kimmel.com>
2017-09-06 12:06:07 -0400

    Speeding up RecommendedBookTest.

--- tests/Feature/RecommendedBookTest.php
+++ tests/Feature/RecommendedBookTest.php
@@ -15,11 +15,11 @@ class RecommendedBookTest extends TestCase
     public function testIndexEndpointReturnsTheBestRatedBooks()
     {
         $book1 = factory(Book::class)->states(['withCategory', 'withAuthor', 'withUser'])->create();
-        $book1->userReviews()->saveMany(factory(UserReview::class, 40)->states(['withUser'])->make(['rating' => 5]));
-        $book1->userReviews()->saveMany(factory(UserReview::class, 10)->states(['withUser'])->make(['rating' => 4]));
+        $book1->userReviews()->saveMany(factory(UserReview::class, 8)->states(['withUser'])->make(['rating' => 5]));
+        $book1->userReviews()->saveMany(factory(UserReview::class, 2)->states(['withUser'])->make(['rating' => 4]));
 
         $book2 = factory(Book::class)->states(['withCategory', 'withAuthor', 'withUser'])->create();
-        $book2->userReviews()->saveMany(factory(UserReview::class, 50)->states(['withUser'])->make(['rating' => 5]));
+        $book2->userReviews()->saveMany(factory(UserReview::class, 10)->states(['withUser'])->make(['rating' => 5]));
 
         $response = $this->getJson("/recommended/books");
 
