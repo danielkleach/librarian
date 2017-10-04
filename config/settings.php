55d4c8c73ab89b227ec8263917189c85d2a20600
Daniel Leach <dleach@kimmel.com>
2017-10-04 14:55:36 -0400

    Adding config settings for TMDB API.

--- config/settings.php
+++ config/settings.php
@@ -11,5 +11,12 @@ return [
     |
     */
 
-    'rental_period' => 14
+    'rental_period' => 14,
+    'tmdb' => [
+        'url' => 'https://api.themoviedb.org/3/search/movie',
+        'api_key' => env('TMDB_API_KEY', null),
+        'images' => [
+            'base_url' => 'https://image.tmdb.org/t/p/w154'
+        ]
+    ],
 ];
