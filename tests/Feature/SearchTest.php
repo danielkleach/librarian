ecdff413b89e1e133b1adca202cad984713343f3
Daniel Leach <dleach@kimmel.com>
2017-10-06 11:06:53 -0400

    Adding a test for video search endpoint.

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Book;
+use App\Video;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -33,7 +34,7 @@ class SearchTest extends TestCase
 
         $data = ['search' => 2200];
 
-        $response = $this->postJson("/search", $data);
+        $response = $this->postJson("/books/search", $data);
 
         $response->assertStatus(200);
         $response->assertJsonFragment([
@@ -44,5 +45,32 @@ class SearchTest extends TestCase
             'publication_year' => (int) $book->publication_year,
         ]);
         $book->unsearchable();
+    }
+
+    public function testIndexEndpointCanSearchForVideos()
+    {
+        $video = factory(Video::class)->create([
+            'title' => 'Title for Search',
+            'description' => 'A good video.',
+            'release_date' => '2017-01-01',
+            'runtime' => 120
+        ]);
+        $video->searchable();
+
+        sleep(1);
+
+        $data = ['search' => 'A good video.'];
+
+        $response = $this->postJson("/videos/search", $data);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'id' => (int) $video->id,
+            'title' => $video->title,
+            'description' => $video->description,
+            'release_date' => $video->release_date,
+            'runtime' => $video->runtime
+        ]);
+        $video->unsearchable();
     }
 }
